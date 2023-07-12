37a342cc3d99cc12f48b382f0ac7d9c795258310
Shesh Anand <sheshanand@local>
2023-07-12 05:35:33 +0100

    first commit

---
+++ src/main/java/com/example/demos/DemossApplication.java
@@ -0,0 +1,13 @@
+package com.example.demos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemossApplication.class, args);
+	}
+
+}
